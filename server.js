ac381e059afc30f7451a732e5452dcdacb95ade9
borderpointer <ellijpark@gmail.com>
2015-12-15 17:10:22 -0500

    testing without cores

--- server.js
+++ server.js
@@ -14,11 +14,11 @@ var app          = express();
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static('www'));
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  next();
-});
+// app.use(function(req, res, next) {
+//   res.header("Access-Control-Allow-Origin", "*");
+//   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+//   next();
+// });
 
 // LISTENER
 
